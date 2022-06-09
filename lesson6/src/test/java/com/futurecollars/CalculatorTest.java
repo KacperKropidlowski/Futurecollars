6b89841f320b94e777a496507f3a0c8f0bf7dfe1
Kacper Kropidłowski <kappip@tlen.pl>
2022-06-09 12:19:37 +0200

    Task 3

---
+++ lesson6/src/test/java/com/futurecollars/CalculatorTest.java
@@ -0,0 +1,31 @@
+package com.futurecollars;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        Assertions.assertEquals(Calculator.add(1,1),2);
+        Assertions.assertEquals(Calculator.add(2,2),4);
+    }
+
+    @Test
+    void subtract() {
+        Assertions.assertEquals(Calculator.subtract(1,1),0);
+        Assertions.assertEquals(Calculator.subtract(4,2),2);
+    }
+
+    @Test
+    void multiply() {
+        Assertions.assertEquals(Calculator.multiply(1,1),1);
+        Assertions.assertEquals(Calculator.multiply(2,2),4);
+    }
+
+    @Test
+    void divide() {
+        Assertions.assertEquals(Calculator.divide(1,1),1);
+        Assertions.assertEquals(Calculator.divide(4,2),2);
+    }
+}
