d56dc2d80a92b9e8d945e1753bf6e0aba9647909
Kacper Kropidłowski <kappip@tlen.pl>
2022-06-14 10:27:20 +0200

    Task 1 Lesson 8

---
+++ lesson8/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        ConsoleMessage consoleMessage1 = new ConsoleMessage();
+        System.out.println(consoleMessage1.getMessage());
+
+        WindowMessage windowMessage1 = new WindowMessage();
+        System.out.println(windowMessage1.getMessage());
+    }
+}
