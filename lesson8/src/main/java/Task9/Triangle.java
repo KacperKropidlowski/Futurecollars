1bd01ae294f81231093a5896b4c1d76ed0987c99
Kacper Kropidłowski <kappip@tlen.pl>
2022-06-17 10:44:50 +0200

    Task 9 Lesson 8

---
+++ lesson8/src/main/java/Task9/Triangle.java
@@ -0,0 +1,19 @@
+package Task9;
+
+public class Triangle {
+    private final int width;
+    private final int height;
+
+    public Triangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
