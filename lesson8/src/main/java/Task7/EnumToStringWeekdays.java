4273df6f1acd01fc390f12443458feb422761d1f
Kacper Kropidłowski <kappip@tlen.pl>
2022-06-17 08:48:23 +0200

    Task 7 Lesson 8

---
+++ lesson8/src/main/java/Task7/EnumToStringWeekdays.java
@@ -0,0 +1,14 @@
+package Task7;
+
+public class EnumToStringWeekdays {
+
+    EnumWeekdays weekday;
+
+    public EnumToStringWeekdays(EnumWeekdays weekday) {
+        this.weekday = weekday;
+    }
+
+    public String enumToStringWeekdays (){
+        return weekday.toString();
+    }
+}
---
+++ lesson8/src/main/java/Task7/EnumWeekdays.java
@@ -0,0 +1,5 @@
+package Task7;
+
+public enum EnumWeekdays {
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
