8592987050318cccfe6b32dd58ed3adb946c0349
Kacper Kropidłowski <kappip@tlen.pl>
2022-06-17 19:20:06 +0200

    Task 4 Lesson 9

---
+++ lesson9/src/main/java/Task4/Employee.java
@@ -0,0 +1,61 @@
+package Task4;
+
+
+public class Employee {
+
+    private final String name;
+    private final String surname;
+    private int age;
+    private double baseSalary;
+    private double bonus;
+    public static final String WHITE_SPACE = " ";
+
+
+    public Employee(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Employee(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Employee(String name, String surname, int age, double baseSalary) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.baseSalary = baseSalary;
+
+    }
+
+    public Employee(String name, String surname, int age, double baseSalary, double bonus) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.baseSalary = baseSalary;
+        this.bonus = bonus;
+    }
+
+    public String getEmployeeInfo() {
+        return "Employee name : " + name + WHITE_SPACE + surname;
+    }
+
+    public String getEmployeeDetails() {
+        return "Employee details : " + name + WHITE_SPACE + surname + " is " + age;
+    }
+
+    public double getTotalSalary() {
+        return baseSalary + bonus;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+}
---
+++ lesson9/src/test/java/Task4/EmployeeTest.java
@@ -0,0 +1,63 @@
+package Task4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+    @Test
+    void shouldRetrieveEmployeeDescription() {
+        //given
+        Employee employee = new Employee("John", "Smith");
+        //when
+        String actualEmployeeInfo = employee.getEmployeeInfo();
+        //then
+        Assertions.assertNotNull(actualEmployeeInfo);
+        Assertions.assertEquals("Employee name : John Smith", actualEmployeeInfo);
+    }
+
+    @Test
+    void shouldRetrieveEmployeeDetailsWithAge() {
+        //given
+        Employee employee = new Employee("John", "Smith", 45);
+        //when
+        String actualEmployeeDetails = employee.getEmployeeDetails();
+        //then
+        Assertions.assertNotNull(actualEmployeeDetails);
+        Assertions.assertEquals("Employee details : John Smith is 45", actualEmployeeDetails);
+    }
+
+
+    @Test
+    void shouldRetrieveBaseSalary() {
+        //given
+        Employee employee = new Employee("John", "Smith", 45, 3000);
+        //when
+        double actualBaseSalary = employee.getBaseSalary();
+        //then
+        Assertions.assertEquals(3000, actualBaseSalary);
+    }
+
+
+    @Test
+    void shouldRetrieveBaseSalaryWithBonus() {
+        //given
+        Employee employee = new Employee("John", "Smith", 45, 3000, 500);
+        //when
+        double actualTotalSalary = employee.getTotalSalary();
+        //then
+        Assertions.assertEquals(3500, actualTotalSalary);
+    }
+
+    @Test
+    void shouldRetrieveEmployeeBonus() {
+        //given
+
+        Employee employee = new Employee("John", "Smith",45,3000,500);
+        //when
+        double actualBonus = employee.getBonus();
+        //then
+        Assertions.assertEquals(500, actualBonus);
+    }
+
+}
