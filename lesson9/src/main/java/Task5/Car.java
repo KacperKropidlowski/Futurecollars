4eed7fb4301f596eb941dd5b29bffbd72d438832
Kacper Kropidłowski <kappip@tlen.pl>
2022-06-18 08:04:59 +0200

    Task 5 Lesson after refactoring

--- lesson9/src/main/java/Task5/Car.java
+++ lesson9/src/main/java/Task5/Car.java
@@ -2,4 +2,26 @@ package Task5;
 
 public class Car extends Vehicle {
 
+    private boolean engineStarted;
+
+    protected void startEngine() {
+        if (!engineStarted) {
+            engineStarted = true;
+        }
+    }
+
+    protected void stopEngine() {
+        if (engineStarted) {
+            engineStarted = false;
+        }
+    }
+
+    public boolean isEngineStarted() {
+        return engineStarted;
+    }
+
+    public void setEngineStarted(boolean engineStarted) {
+        this.engineStarted = engineStarted;
+    }
+
 }
