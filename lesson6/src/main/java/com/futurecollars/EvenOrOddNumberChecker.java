f1376bc3f2fb6902915b9e5f5bf718453103b74c
Kacper Kropidłowski <kappip@tlen.pl>
2022-06-23 17:26:00 +0200

    Task 5 Lesson 6 correction

---
+++ lesson6/src/main/java/com/futurecollars/EvenOrOddNumberChecker.java
@@ -0,0 +1,12 @@
+package com.futurecollars;
+
+public class EvenOrOddNumberChecker {
+    public static boolean checkIfNumberIsEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static boolean checkIfNumberIsOdd(int number) {
+        return number % 2 != 0;
+    }
+}
+
