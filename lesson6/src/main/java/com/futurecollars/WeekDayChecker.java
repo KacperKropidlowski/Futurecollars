37dea8428c6dcfcb8c9dd1075eb70d0d26152818
Kacper Kropidłowski <kappip@tlen.pl>
2022-06-24 08:59:05 +0200

    Task 6 Lesson 6 correction

---
+++ lesson6/src/main/java/com/futurecollars/WeekDayChecker.java
@@ -0,0 +1,23 @@
+package com.futurecollars;
+
+public class WeekDayChecker {
+    public static String checkWeekday(int day) {
+        switch (day) {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+            case 7:
+                return "Weekend";
+
+        }
+        return "There is no such day!";
+    }
+}
