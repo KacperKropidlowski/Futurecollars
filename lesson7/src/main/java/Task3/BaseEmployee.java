4f99b7b097f2c25627860676fcd8b17a1d2a0f9f
Kacper Kropidłowski <kappip@tlen.pl>
2022-06-24 12:52:23 +0200

    Task 3 Lesson 7 correction

--- lesson7/src/main/java/Task3/BaseEmployee.java
+++ lesson7/src/main/java/Task3/BaseEmployee.java
@@ -1,9 +1,10 @@
+package Task3;
+
 public abstract class BaseEmployee {
     protected String name;
     protected String lastname;
     protected int baseSalary = 3000;
-    public int bonusSalary;
-    protected int employmentYear;
+    protected final int employmentYear;
 
     public BaseEmployee(String name, String lastname, int employmentYear) {
         this.name = name;
@@ -11,24 +12,17 @@ public abstract class BaseEmployee {
         this.employmentYear = employmentYear;
     }
 
-    public BaseEmployee(String name, String lastname, int employmentYear, int bonusSalary) {
-        this.name = name;
-        this.lastname = lastname;
-        this.employmentYear = employmentYear;
-        this.bonusSalary = bonusSalary;
+    public int getBaseSalary() {
+        return baseSalary;
     }
 
     protected int calculateMonthlySalary() {
-        return baseSalary + bonusSalary;
+        return baseSalary;
     }
 
-    protected int employmentLength() {
+    protected int getEmploymentLength() {
         int year = 2022;
         return year - employmentYear;
-    }
-
-    public int getBaseSalary() {
-        return baseSalary;
     }
 }
 
