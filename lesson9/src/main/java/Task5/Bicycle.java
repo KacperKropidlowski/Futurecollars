d001756d45e2be03d09401f6edd740a9c24d1c55
Kacper Kropidłowski <kappip@tlen.pl>
2022-06-30 13:00:49 +0200

    Task 5 Lesson 9 correction

--- lesson9/src/main/java/Task5/Bicycle.java
+++ lesson9/src/main/java/Task5/Bicycle.java
@@ -1,5 +1,26 @@
 package Task5;
 
-public class Bicycle extends Vehicle {
+public class Bicycle implements Vehicle {
 
+    private boolean isBicycleRiding = false;
+
+    public void setBicycleRiding(boolean bicycleRiding) {
+        isBicycleRiding = bicycleRiding;
+    }
+
+    @Override
+    public void start() {
+        setBicycleRiding(true);
+        System.out.println("Enjoy Your bike ride!");
+    }
+
+    @Override
+    public void stop() {
+        if (isBicycleRiding){
+            setBicycleRiding(false);
+            System.out.println("Bicycle stopped!");
+        } else {
+            System.out.println("It is impossible to stop a bicycle that is not riding!");
+        }
+    }
 }
