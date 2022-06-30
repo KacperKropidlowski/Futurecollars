e8083d847b942785bee889a4aa60253e03fefc01
Kacper Kropidłowski <kappip@tlen.pl>
2022-06-30 10:01:11 +0200

    Task 3 Lesson 9 correction

---
+++ lesson9/src/main/java/Task3/Subtraction.java
@@ -0,0 +1,10 @@
+package Task3;
+
+public class Subtraction implements CalculatorOperation {
+
+    @Override
+    public double calculate(double left, double right) {
+        return left - right;
+    }
+}
+
