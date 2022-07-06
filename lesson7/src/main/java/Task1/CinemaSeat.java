0f7fed7d4cb884412b6b5c9f56066323ccaf1ef4
Kacper Kropidłowski <kappip@tlen.pl>
2022-07-06 12:48:29 +0200

    Cinema seat correction

--- lesson7/src/main/java/Task1/CinemaSeat.java
+++ lesson7/src/main/java/Task1/CinemaSeat.java
@@ -3,14 +3,15 @@ package Task1;
 public class CinemaSeat {
     private final int number;
     private final int row;
-    static boolean isTaken;
+    private boolean isTaken;
 
     public CinemaSeat(int number, int row) {
         this.number = number;
         this.row = row;
         this.isTaken = false;
     }
-    public static void setIsTaken(boolean isTaken) {
-        CinemaSeat.isTaken = isTaken;
+
+    public void setTaken(boolean taken) {
+        isTaken = taken;
     }
 }
