bbf20402fe456c22aea194e98abaa268bc524e7d
Kacper Kropidłowski <kappip@tlen.pl>
2022-07-07 17:18:47 +0200

    Date of birth analyzer

---
+++ lesson14/src/main/java/Task6/DateOfBirth.java
@@ -0,0 +1,34 @@
+package Task6;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DateOfBirth {
+
+    public void analyzeDateOfBirth() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter date your date of birth in format YYYY-MM-DD:");
+        String dateOfBirthInput = scanner.nextLine();
+
+        LocalDate dateNow = LocalDate.now();
+        Pattern datePattern = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
+        Matcher dateMatcher = datePattern.matcher(dateOfBirthInput);
+
+        boolean matches = dateMatcher.find();
+        if (matches){
+            LocalDate dateOfBirth = LocalDate.parse(dateOfBirthInput);
+            System.out.println("You are " + (dateNow.getYear()-dateOfBirth.getYear()) + " years old.");
+            System.out.println("You were born on " + dateOfBirth.getDayOfWeek().toString().toLowerCase() + ".");
+            System.out.println("You were born in " + dateOfBirth.getDayOfYear()/7 + " week of the year.");
+        } else {
+            System.out.println("Wrong format of date!");
+        }
+    }
+
+    public static void main(String[] args) {
+        DateOfBirth s = new DateOfBirth();
+        s.analyzeDateOfBirth();
+    }
+}
