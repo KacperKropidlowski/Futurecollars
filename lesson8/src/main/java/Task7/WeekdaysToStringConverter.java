1d41a8ffaeada7fbebf7ba438533f619aadc4a35
Kacper Kropidłowski <kappip@tlen.pl>
2022-07-11 15:34:11 +0200

    Weekdays to string converter correction

--- lesson8/src/main/java/Task7/WeekdaysToStringConverter.java
+++ lesson8/src/main/java/Task7/WeekdaysToStringConverter.java
@@ -3,7 +3,11 @@ package Task7;
 public class WeekdaysToStringConverter {
 
     public String convertWeekdayToString(Weekdays weekdays) {
-        return weekdays.toString();
+        return capitalize(weekdays.name().toLowerCase());
+    }
+
+    private String capitalize(String text) {
+        return text.substring(0, 1).toUpperCase() + text.substring(1);
     }
 }
 
