b1c239c0f3fcb8e3d8b82f3f1be004837222eb78
Kacper Kropidłowski <kappip@tlen.pl>
2022-07-11 15:14:36 +0200

    Hopefully last text correction

--- lesson9/src/main/java/Task1/Text.java
+++ lesson9/src/main/java/Task1/Text.java
@@ -1,27 +1,29 @@
 package Task1;
 
 public class Text {
+
+    public void analyzeText(String text) {
+        if (hasContent(text)) {
+            printIfPalindrome(text);
+            printLength(text);
+        } else System.out.println("Text is blank!");
+    }
+
     private boolean hasContent(String text) {
         return !text.isBlank();
     }
 
     private boolean isPalindrome(String text) {
-        return text.toLowerCase().equals(new StringBuilder(text.toLowerCase()).reverse().toString());
+        String textInLowerCase = text.toLowerCase();
+        return textInLowerCase.equals(new StringBuilder(textInLowerCase).reverse().toString());
     }
 
-    private void getLength(String text) {
+    private void printLength(String text) {
         System.out.println("Text length: " + text.length());
     }
 
-    private void getPalindrome(String text) {
+    private void printIfPalindrome(String text) {
         System.out.println("Text is palindrome: " + isPalindrome(text));
-    }
-
-    public void analyzeText(String text) {
-        if (hasContent(text)) {
-            getPalindrome(text);
-            getLength(text);
-        } else throw new IllegalArgumentException("Text is blank!");
     }
 }
 
