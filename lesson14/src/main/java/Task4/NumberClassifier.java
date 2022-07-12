5d202ca7832ef7f4e959538d83da3fc9c1849cde
Kacper Kropidłowski <kappip@tlen.pl>
2022-07-12 12:26:36 +0200

    Number classifier correction

--- lesson14/src/main/java/Task4/NumberClassifier.java
+++ lesson14/src/main/java/Task4/NumberClassifier.java
@@ -1,14 +1,18 @@
 package Task4;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NumberClassifier {
+    private final static Pattern integersPattern = Pattern.compile("^(\\d+)$");
+    private final static Pattern floatingPointNumbersPattern = Pattern.compile("^(\\d+)\\.(\\d+)$");
+    private final static Pattern numbersInScientificNotationPattern = Pattern.compile("^\\d\\.(\\d)+[eE][+-](\\d)+$");
 
     public static void main(String[] args) {
         NumberClassifier numberClassifier = new NumberClassifier();
-        ArrayList<String> numbers = new ArrayList<>();
+        List<String> numbers = new ArrayList<>();
         numbers.add("342");
         numbers.add("5.34");
         numbers.add("756");
@@ -26,15 +30,14 @@ public class NumberClassifier {
 
     }
 
-    public void classifyNumbers(ArrayList<String> numbers) {
+    public void classifyNumbers(List<String> numbers) {
         System.out.println("Arguments classification:\nIntegers: " + getIntegers(numbers)
                 + "\nFloating Point Numbers: " + getFloatingPointNumbers(numbers)
                 + "\nNumbers in scientific notation: " + getNumbersInScientificNotation(numbers));
     }
 
-    public ArrayList<String> getIntegers(ArrayList<String> numbers) {
-        ArrayList<String> integers = new ArrayList<>();
-        Pattern integersPattern = Pattern.compile("^(\\d+)$");
+    public List<String> getIntegers(List<String> numbers) {
+        List<String> integers = new ArrayList<>();
         for (String number : numbers) {
             Matcher integersMatcher = integersPattern.matcher(number);
             boolean matchesIntegers = integersMatcher.matches();
@@ -45,9 +48,8 @@ public class NumberClassifier {
         return integers;
     }
 
-    public ArrayList<String> getFloatingPointNumbers(ArrayList<String> numbers) {
-        ArrayList<String> floatingPointNumbers = new ArrayList<>();
-        Pattern floatingPointNumbersPattern = Pattern.compile("^(\\d+)\\.(\\d+)$");
+    public List<String> getFloatingPointNumbers(List<String> numbers) {
+        List<String> floatingPointNumbers = new ArrayList<>();
         for (String number : numbers) {
             Matcher floatingPointNumbersMatcher = floatingPointNumbersPattern.matcher(number);
             boolean matchesFloatingPointNumbers = floatingPointNumbersMatcher.matches();
@@ -58,9 +60,8 @@ public class NumberClassifier {
         return floatingPointNumbers;
     }
 
-    public ArrayList<String> getNumbersInScientificNotation(ArrayList<String> numbers) {
-        ArrayList<String> numbersInScientificNotation = new ArrayList<>();
-        Pattern numbersInScientificNotationPattern = Pattern.compile("^\\d\\.(\\d)+[eE][+-](\\d)+$");
+    public List<String> getNumbersInScientificNotation(List<String> numbers) {
+        List<String> numbersInScientificNotation = new ArrayList<>();
         for (String number : numbers) {
             Matcher numbersInScientificNotationMatcher = numbersInScientificNotationPattern.matcher(number);
             boolean matchesNumbersInScientificNotation = numbersInScientificNotationMatcher.matches();
