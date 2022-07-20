e33bb9d37df53c525e69352a1fc5ebabc871a740
Kacper Kropidłowski <kappip@tlen.pl>
2022-07-20 20:05:43 +0200

    String decomposer

--- lesson15/src/main/java/Task5/StringDecomposer.java
+++ lesson15/src/main/java/Task5/StringDecomposer.java
@@ -1,6 +1,6 @@
 package Task5;
 
-import java.util.HashSet;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -8,17 +8,17 @@ import java.util.stream.Collectors;
 public class StringDecomposer {
 
     public Set<Character> decomposeStrings(List<String> strings) {
-        Set<Character> decomposedStrings = new HashSet<>();
-        strings.
-                forEach(s -> decomposedStrings.addAll(convertStringToCharList(s.toUpperCase())));
-
-        return decomposedStrings;
+        return strings
+                .stream()
+                .map(this::convertStringToCharList)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
     }
 
-    public List<Character> convertStringToCharList(String string){
+    private List<Character> convertStringToCharList(String string) {
         return string
                 .chars()
-                .mapToObj(c -> (char)c )
+                .mapToObj(c -> (char) c)
                 .collect(Collectors.toList());
     }
 }
