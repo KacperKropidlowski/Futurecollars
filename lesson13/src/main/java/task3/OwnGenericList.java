15ff63b969964952adb505d0eb426337972fb74f
Kacper Kropidłowski <kappip@tlen.pl>
2022-07-22 11:17:53 +0200

    Own generic list

--- lesson13/src/main/java/task3/OwnGenericList.java
+++ lesson13/src/main/java/task3/OwnGenericList.java
@@ -1,12 +1,22 @@
 package task3;
 
-public class OwnGenericList<E> implements OwnList<E>{
+public class OwnGenericList<E> implements OwnList<E> {
 
-    private final Object[] elements = new Object[10];
+    private Object[] elements = new Object[10];
 
     @Override
     public boolean add(E e) {
-        return false;
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == null) {
+                elements[i] = e;
+                return true;
+            }
+        }
+        Object[] temp = new Object[elements.length * 2];
+        System.arraycopy(elements, 0, temp, 0, elements.length);
+        temp[elements.length] = e;
+        elements = temp;
+        return true;
     }
 
     @Override
@@ -16,11 +26,17 @@ public class OwnGenericList<E> implements OwnList<E>{
 
     @Override
     public boolean remove(E o) {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i].equals(o)) {
+                elements[i] = null;
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.elements.length;
     }
 }
