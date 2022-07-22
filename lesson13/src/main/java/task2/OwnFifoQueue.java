298314cb9b9234e453350b3be8115eae0fd43073
Kacper Kropidłowski <kappip@tlen.pl>
2022-07-22 11:52:08 +0200

    Own fifo queue correction

--- lesson13/src/main/java/task2/OwnFifoQueue.java
+++ lesson13/src/main/java/task2/OwnFifoQueue.java
@@ -1,27 +1,34 @@
 package task2;
 
 public class OwnFifoQueue implements OwnQueue {
-    Integer[] ownFifoQueue = new Integer[2];
+    private Integer[] ownFifoQueue = new Integer[2];
 
     @Override
-    public boolean add(Integer e) {
+    public boolean add(Integer element) {
         for (int i = 0; i < ownFifoQueue.length; i++) {
             if (ownFifoQueue[i] == null) {
-                ownFifoQueue[i] = e;
+                ownFifoQueue[i] = element;
                 return true;
             }
         }
         Integer[] temp = new Integer[ownFifoQueue.length * 2];
         System.arraycopy(ownFifoQueue, 0, temp, 0, ownFifoQueue.length);
-        temp[ownFifoQueue.length] = e;
+        temp[ownFifoQueue.length] = element;
         ownFifoQueue = temp;
         return true;
     }
 
     @Override
     public Integer remove() {
+        int halfSize = ownFifoQueue.length / 2;
         Integer removed = ownFifoQueue[0];
         System.arraycopy(ownFifoQueue, 1, ownFifoQueue, 0, ownFifoQueue.length - 1);
+
+        if (ownFifoQueue[halfSize] == null) {
+            Integer[] temp = new Integer[halfSize];
+            System.arraycopy(ownFifoQueue, 0, ownFifoQueue, 0, halfSize);
+            ownFifoQueue = temp;
+        }
         return removed;
     }
 
